aafcf105ca6a84b0ab44c0661e545c06af1ee5a1
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-04-18 17:00:01 +0100

    adding base project

---
+++ src/main/java/com/aledev/alba/msbnbbookingservice/MsBnbBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBnbBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBnbBookingServiceApplication.class, args);
+    }
+
+}
