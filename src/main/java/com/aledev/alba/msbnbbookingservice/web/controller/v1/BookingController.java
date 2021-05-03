d5a9a9e9c3086edbe628592ced57d97e1a716abc
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-05-03 13:15:26 +0100

    Adding OpenAPI v3.0 (Swagger)

--- src/main/java/com/aledev/alba/msbnbbookingservice/web/controller/v1/BookingController.java
+++ src/main/java/com/aledev/alba/msbnbbookingservice/web/controller/v1/BookingController.java
@@ -3,6 +3,7 @@ package com.aledev.alba.msbnbbookingservice.web.controller.v1;
 import com.aledev.alba.msbnbbookingservice.domain.BookingPagedList;
 import com.aledev.alba.msbnbbookingservice.service.BookingService;
 import com.aledev.alba.msbnbbookingservice.web.model.BookingDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,7 @@ public class BookingController {
     private final BookingService bookingService;
 
     @GetMapping
+    @Operation(summary = "Fetch all bookings")
     public BookingPagedList listBookings(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
                                          @RequestParam(value = "pageSize", required = false) Integer pageSize) {
         if (pageNumber == null || pageNumber < 0){
@@ -36,28 +38,33 @@ public class BookingController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add new booking")
     public BookingDto placeBooking(@RequestBody @Valid BookingDto bookingDto) {
         return bookingService.newBooking(bookingDto);
     }
 
     @GetMapping("/{bookingId}")
+    @Operation(summary = "Get booking by ID")
     public BookingDto getBookingById(@PathVariable Long bookingId) {
         return bookingService.getBookingById(bookingId);
     }
 
     @GetMapping("/uuid/{bookingUUID}")
+    @Operation(summary = "Get booking by UUID")
     public BookingDto getBookingByUuId(@PathVariable UUID bookingUUID) {
         return bookingService.getBookingByUUID(bookingUUID);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update existing booking")
     public BookingDto updateBooking(@PathVariable Long id, @RequestBody BookingDto bookingDto) {
         return bookingService.updateBooking(id, bookingDto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete one booking")
     public void deleteBooking(@PathVariable Long id) {
         bookingService.deleteBooking(id);
     }
